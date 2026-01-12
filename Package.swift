4512363daa7e4005651f0779e6c8bc27d85ae4b6
zunda <zunda.dev@gmail.com>
2026-01-12 20:07:51 +0900

    add MemberwizeInit Package

--- Package.swift
+++ Package.swift
@@ -17,6 +17,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/apple/swift-http-types", from: "1.3.0"),
     .package(url: "https://github.com/zunda-pixel/http-client", from: "0.3.0"),
+    .package(url: "https://github.com/gohanlon/swift-memberwise-init-macro.git", from: "0.5.2"),
   ],
   targets: [
     .target(
@@ -25,6 +26,7 @@ let package = Package(
         .product(name: "HTTPTypes", package: "swift-http-types"),
         .product(name: "HTTPTypesFoundation", package: "swift-http-types"),
         .product(name: "HTTPClient", package: "http-client"),
+        .product(name: "MemberwiseInit", package: "swift-memberwise-init-macro"),
       ]
     ),
     .testTarget(
